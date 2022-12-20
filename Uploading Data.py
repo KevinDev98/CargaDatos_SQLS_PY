87b97bc34fccedf1d2eb3e3bfb41bd7480e76d32
Kevin Gutierrez Eliosa <108048485+KevinDev98@users.noreply.github.com>
2022-12-19 18:18:16 -0600

    Comments

--- Uploading Data.py
+++ Uploading Data.py
@@ -6,13 +6,13 @@ Created on Mon Dec 19 13:04:35 2022
 import pyodbc
 import pandas as pd
 
-df=pd.read_csv('C:\\Users\Kevin\Documents\KEVIN\PROYECTOS DEV\CARGA DATOS\DATA FILES\CARGA_CSV\CIUDAD_CSV.csv')
+df=pd.read_csv('C:\\Users\Kevin\Documents\KEVIN\PROYECTOS DEV\CARGA DATOS\DATA FILES\CARGA_CSV\CIUDAD_CSV.csv') #Lee el archivo
 srvr ='DESKTOP-EB8Q5MG'
 db='VENTAS_CIUDAD'
 username='sa'
 pwd='inginf98'
 try:    
-    StrConnect='DRIVER={SQL Server};SERVER='+srvr+';DATABASE='+db+';UID='+username+';PWD='+pwd
+    StrConnect='DRIVER={SQL Server};SERVER='+srvr+';DATABASE='+db+';UID='+username+';PWD='+pwd #Se establecen parametros de conexión
     #print(StrConnect)
     Connection=pyodbc.connect(StrConnect)
     print('Conexion establecida con SQL SERVER')
@@ -37,7 +37,8 @@ cursor = Connection.cursor()
 try:    
      for index, row in df.iterrows(): #Recorre las lineas del csv
          try:        
-             # print('inicia proceso')
+             # print('inicia proceso') 
+             #Se inicia limpieza de datos
              id_city=Remove_spacewith(row.VNT_CVE_CIUDAD)
              id_region=Remove_spacewith(row.VNT_CVE_REGION)
              nombreCity=Remove_spacewith(row.VNT_NOMBRE_CIUDAD)             
@@ -45,10 +46,12 @@ try:
              id_region=id_region.strip()
              nombreCity=nombreCity.strip()
              #print("id_city:"+ id_city + "id_region:" + id_region + "nombre ciudad:" + nombreCity)
+             #Para este punto ya se limpiaron los datos
+            
              cursor.execute("INSERT INTO VNT_CAT_CIUDADES(VNT_CVE_CIUDAD,VNT_CVE_REGION,VNT_NOMBRE_CIUDAD)VALUES(?,?,?)", id_city, id_region, nombreCity)
-             print('Registro insertado')
+             print('Registro insertado correctamente')
          except Exception as ex:
-             print('Error loading data: {}' .format(ex))  
+             print('Error loading data: {}' .format(ex))  #Control de excepciones
              #cursor.close()        
 except Exception as ex:    
     print('Error Method: {}' .format(ex)) 
